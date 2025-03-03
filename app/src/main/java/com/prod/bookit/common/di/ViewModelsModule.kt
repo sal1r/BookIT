56bc803eee40ae4d791d014f2a76e257c32a6766
PotaninPM <potaninpm@gmail.com>
2025-03-03 15:42:39 +0300

    removing single from di

--- app/src/main/java/com/prod/bookit/common/di/ViewModelsModule.kt
+++ app/src/main/java/com/prod/bookit/common/di/ViewModelsModule.kt
@@ -6,16 +6,17 @@ import com.prod.bookit.presentation.viewModels.CoworkingsViewModel
 import com.prod.bookit.presentation.viewModels.ProfileViewModel
 import com.prod.bookit.presentation.viewModels.SettingsViewModel
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
 
-    single { AuthViewModel(get()) }
+    viewModel { AuthViewModel(get()) }
 
     single { SettingsViewModel(get(), get(), get()) }
 
-    single { CoworkingsViewModel(get()) }
+    viewModel { CoworkingsViewModel(get()) }
 
     single { ProfileViewModel(get()) }
 
